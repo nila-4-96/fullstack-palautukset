4f21833d9bf8d06e573c3dcfcc27cbd2ead5fb71
Litja Niko S <niklitja@cubbli24-gold.cs.helsinki.fi>
2026-03-24 15:23:34 +0200

    Tehtävä 1.6

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+    <h1>give feedback</h1>
+    <button onClick={() => {
+      setGood(good + 1)
+      console.log('good')
+    }}>
+      good
+    </button>
+    <button onClick={() => {
+      setNeutral(neutral + 1)
+      console.log('neutral')
+    }}>
+      neutral
+    </button>
+    <button onClick={() => {
+      setBad(bad + 1)
+      console.log('bad')
+    }}>
+      bad
+    </button>
+
+    <h1>statistics</h1>
+    <p>good {good}<br />
+    neutral {neutral}<br />
+    bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
