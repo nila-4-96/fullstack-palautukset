02af206cba2ad8d4625043ea637aa36cafb915c8
nila-4-96 <niko.s.litja@helsinki.fi>
2026-03-30 16:04:38 +0300

    Tehtävät 2.3, 2.4

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -21,9 +21,9 @@ const Content = (props) => {
   /*console.log("Content", props)*/
   return (
     <div>
-      {props.parts.map(part => (
+      {props.parts.map(part => 
         <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
+      )}
     </div>
   )
 }
@@ -32,9 +32,9 @@ const Header = (props) => {
   /*console.log("Header", props)*/
   return (  
     <div>
-      <h1>
+      <h2>
         {props.course}
-      </h1>
+      </h2>
     </div>
    )
 }
@@ -51,31 +51,57 @@ const Course = (props) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
     </div>
   )
 }
