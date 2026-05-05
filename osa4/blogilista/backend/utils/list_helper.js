8e9ab39d2d88157f789871c12335d075566f523f
nila-4-96 <niko.s.litja@helsinki.fi>
2026-05-05 18:33:08 +0300

    Tehtävä 4.5

--- osa4/blogilista/backend/utils/list_helper.js
+++ osa4/blogilista/backend/utils/list_helper.js
@@ -9,7 +9,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (max, item) => {
+    if (item.likes > max.likes) {
+      return item
+    } else {
+      return max
+    }
+  }
+  return {
+    title: blogs.reduce(reducer).title,
+    author: blogs.reduce(reducer).author,
+    url: blogs.reduce(reducer).url,
+    likes: blogs.reduce(reducer).likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
