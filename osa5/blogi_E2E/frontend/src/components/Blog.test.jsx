f299cb04667d841bed8da7de9ce58d74dc46d66e
nila-4-96 <niko.s.litja@helsinki.fi>
2026-05-26 17:50:33 +0300

    Tehtävä 5.27

--- osa5/blogi_E2E/frontend/src/components/Blog.test.jsx
+++ osa5/blogi_E2E/frontend/src/components/Blog.test.jsx
@@ -2,18 +2,34 @@ import {
   render,
   screen
 } from '@testing-library/react'
+import {
+  Routes,
+  Route,
+  useParams,
+  useNavigate,
+  BrowserRouter as Router
+} from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import BlogList from './BlogList'
+import BlogForm from './BlogForm'
 
-test('only title and author are rendered at start', async () => {
-
+test('all details but not buttons are shown (logged out)', async () => {
   const blog = {
     title: 'Rendered title',
     author: 'Pim',
-    url: 'gogel.co.ru.xyz'
+    url: 'gogel.co.ru.xyz',
+    user: {
+      name: 'Magabur',
+      username: 'magabur'
+    }
   }
 
-  const { container } = render(<Blog blog={blog} />)
+  const { container } = render(
+    <Router>
+      <Blog blog={blog} />
+    </Router>
+  )
 
   const div = container.querySelector('.blog')
 
@@ -25,15 +41,124 @@ test('only title and author are rendered at start', async () => {
     'Pim'
   )
 
-  expect(div).not.toHaveTextContent(
+  expect(div).toHaveTextContent(
     'gogel.co.ru.xyz'
   )
 
-  expect(div).not.toHaveTextContent(
+  expect(div).toHaveTextContent(
     'likes'
   )
+
+  expect(div).toHaveTextContent(
+    'Magabur'
+  )
+
+  const likeButton = screen.queryAllByText('like')
+  expect(likeButton).toHaveLength(0)
+
+  const removeButton = screen.queryAllByText('remove')
+  expect(removeButton).toHaveLength(0)
 })
 
+test('all details + like button are shown (logged in, another user)', async () => {
+  const blog = {
+    title: 'Rendered title',
+    author: 'Pim',
+    url: 'gogel.co.ru.xyz',
+    user: {
+      name: 'Magabur',
+      username: 'magabur'
+    }
+  }
+
+  const secUser = {
+    name: 'Bungley',
+    username: 'spungley'
+  }
+
+  const { container } = render(
+    <Router>
+      <Blog blog={blog} user={secUser} />
+    </Router>
+  )
+
+  const div = container.querySelector('.blog')
+
+  expect(div).toHaveTextContent(
+    'Rendered title'
+  )
+
+  expect(div).toHaveTextContent(
+    'Pim'
+  )
+
+  expect(div).toHaveTextContent(
+    'gogel.co.ru.xyz'
+  )
+
+  expect(div).toHaveTextContent(
+    'likes'
+  )
+
+  expect(div).toHaveTextContent(
+    'Magabur'
+  )
+
+  const likeButton = screen.queryAllByText('like')
+  expect(likeButton).toHaveLength(1)
+
+  const removeButton = screen.queryAllByText('remove')
+  expect(removeButton).toHaveLength(0)
+})
+
+test('all details + like and remove buttons are shown (logged in, blog poster)', async () => {
+  const blog = {
+    title: 'Rendered title',
+    author: 'Pim',
+    url: 'gogel.co.ru.xyz',
+    user: {
+      name: 'Magabur',
+      username: 'magabur'
+    }
+  }
+
+  const { container } = render(
+    <Router>
+      <Blog blog={blog} user={blog.user} />
+    </Router>
+  )
+
+  const div = container.querySelector('.blog')
+
+  expect(div).toHaveTextContent(
+    'Rendered title'
+  )
+
+  expect(div).toHaveTextContent(
+    'Pim'
+  )
+
+  expect(div).toHaveTextContent(
+    'gogel.co.ru.xyz'
+  )
+
+  expect(div).toHaveTextContent(
+    'likes'
+  )
+
+  expect(div).toHaveTextContent(
+    'Magabur'
+  )
+
+  const likeButton = screen.queryAllByText('like')
+  expect(likeButton).toHaveLength(1)
+
+  const removeButton = screen.queryAllByText('remove')
+  expect(removeButton).toHaveLength(1)
+})
+
+
+/*
 test('url, likes and user are shown when the view button is clicked', async () => {
 
   const blog = {
@@ -99,3 +224,4 @@ test('like button can be clicked twice', async () => {
 
   expect(mockHandler.mock.calls).toHaveLength(2)
 })
+*/
